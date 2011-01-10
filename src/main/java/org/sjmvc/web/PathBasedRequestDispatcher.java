40f7380639f843b2a33a88abbdbbdee0f2af9a81
Ignasi Barrera <ignasi.barrera@gmail.com>
2011-01-10 08:31:20 +0100

    Fixed logging

--- src/main/java/org/sjmvc/web/PathBasedRequestDispatcher.java
+++ src/main/java/org/sjmvc/web/PathBasedRequestDispatcher.java
@@ -39,135 +39,124 @@ import org.slf4j.LoggerFactory;
  * Base class for the {@link RequestDispatcher} implementations.
  * 
  * @author Ignasi Barrera
- * 
  */
 public class PathBasedRequestDispatcher implements RequestDispatcher
 {
-	/** The logger. */
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(PathBasedRequestDispatcher.class);
+    /** The logger. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(PathBasedRequestDispatcher.class);
 
-	/** Mappings from request path to {@link Controller} class objects. */
-	protected Map<String, Class<Controller>> controllerClasses;
+    /** Mappings from request path to {@link Controller} class objects. */
+    protected Map<String, Class<Controller>> controllerClasses;
 
-	/**
-	 * Creates the request dispatcher.
-	 * 
-	 * @throws ConfigurationException If controller mapping configuration cannot
-	 *             be read.
-	 */
-	public PathBasedRequestDispatcher() throws ConfigurationException
-	{
-		super();
-		controllerClasses = new HashMap<String, Class<Controller>>();
-		readConfiguration();
-	}
+    /**
+     * Creates the request dispatcher.
+     * 
+     * @throws ConfigurationException If controller mapping configuration cannot be read.
+     */
+    public PathBasedRequestDispatcher() throws ConfigurationException
+    {
+        super();
+        controllerClasses = new HashMap<String, Class<Controller>>();
+        readConfiguration();
+    }
 
-	@Override
-	public void dispatch(HttpServletRequest req, HttpServletResponse resp)
-			throws Exception
-	{
-		LOGGER.debug("Looking up for a controller to handle request to: {}",
-				req.getRequestURI());
+    @Override
+    public void dispatch(HttpServletRequest req, HttpServletResponse resp) throws Exception
+    {
+        LOGGER.debug("Looking up for a controller to handle request to: {}", req.getRequestURI());
 
-		String controllerPath = null;
-		Class<Controller> controllerClass = null;
-		String requestedPath = getRequestedPath(req);
+        String controllerPath = null;
+        Class<Controller> controllerClass = null;
+        String requestedPath = getRequestedPath(req);
 
-		for (String path : controllerClasses.keySet())
-		{
-			if (requestedPath.startsWith(path))
-			{
-				controllerPath = path;
-				controllerClass = controllerClasses.get(path);
+        for (String path : controllerClasses.keySet())
+        {
+            if (requestedPath.startsWith(path))
+            {
+                controllerPath = path;
+                controllerClass = controllerClasses.get(path);
 
-				LOGGER.debug("Using {} controller to handle request to: {}",
-						controllerClass.getName(), req.getRequestURI());
-			}
-		}
+                LOGGER.debug("Using {} controller to handle request to: {}", controllerClass
+                    .getName(), req.getRequestURI());
+            }
+        }
 
-		if (controllerClass != null)
-		{
-			// Instantiate the controller on each request to make it thread-safe
-			Controller controller = controllerClass.newInstance();
-			String viewName = controller.execute(req, resp);
-			String viewPath = Configuration.VIEW_PATH + controllerPath + "/"
-					+ viewName + Configuration.VIEW_SUFFIX;
+        if (controllerClass != null)
+        {
+            // Instantiate the controller on each request to make it thread-safe
+            Controller controller = controllerClass.newInstance();
+            String viewName = controller.execute(req, resp);
+            String viewPath =
+                Configuration.VIEW_PATH + controllerPath + "/" + viewName
+                    + Configuration.VIEW_SUFFIX;
 
-			req.setAttribute("currentView", viewPath);
-		}
-		else
-		{
-			LOGGER.error("No controller was found to handle request to: {}",
-					req.getRequestURI());
+            req.setAttribute("currentView", viewPath);
+        }
+        else
+        {
+            LOGGER.error("No controller was found to handle request to: {}", req.getRequestURI());
 
-			resp.sendError(
-					HttpServletResponse.SC_NOT_FOUND,
-					"No controller was found to handle request to: "
-							+ req.getRequestURI());
-		}
-	}
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND,
+                "No controller was found to handle request to: " + req.getRequestURI());
+        }
+    }
 
-	/**
-	 * Get the requested path relative to the servlet path.
-	 * 
-	 * @param req The request.
-	 * @return The requested path.
-	 */
-	private String getRequestedPath(final HttpServletRequest req)
-	{
-		return req.getRequestURI().replaceFirst(req.getContextPath(), "")
-				.replaceFirst(req.getServletPath(), "");
-	}
+    /**
+     * Get the requested path relative to the servlet path.
+     * 
+     * @param req The request.
+     * @return The requested path.
+     */
+    private String getRequestedPath(final HttpServletRequest req)
+    {
+        return req.getRequestURI().replaceFirst(req.getContextPath(), "").replaceFirst(
+            req.getServletPath(), "");
+    }
 
-	/**
-	 * Load configured controller mappings.
-	 * 
-	 * @throws Exception If mappings cannot be loaded.
-	 */
-	protected void readConfiguration() throws ConfigurationException
-	{
-		Properties config = Configuration.getConfiguration();
+    /**
+     * Load configured controller mappings.
+     * 
+     * @throws Exception If mappings cannot be loaded.
+     */
+    protected void readConfiguration() throws ConfigurationException
+    {
+        Properties config = Configuration.getConfiguration();
 
-		LOGGER.info("Loading controller mappings...");
+        LOGGER.info("Loading controller mappings...");
 
-		for (Object mappingKey : config.keySet())
-		{
-			String key = (String) mappingKey;
+        for (Object mappingKey : config.keySet())
+        {
+            String key = (String) mappingKey;
 
-			if (Configuration.isControllerProperty(key))
-			{
-				String path = config.getProperty(key);
-				String clazz = config.getProperty(key.replace(
-						Configuration.CONTROLLER_PATH_SUFFIX,
-						Configuration.CONTROLLER_CLASS_SUFFIX));
+            if (Configuration.isControllerProperty(key))
+            {
+                String path = config.getProperty(key);
+                String clazz =
+                    config.getProperty(key.replace(Configuration.CONTROLLER_PATH_SUFFIX,
+                        Configuration.CONTROLLER_CLASS_SUFFIX));
 
-				if (clazz == null)
-				{
-					throw new ConfigurationException(
-							"Missing controller class for path: " + path);
-				}
+                if (clazz == null)
+                {
+                    throw new ConfigurationException("Missing controller class for path: " + path);
+                }
 
-				try
-				{
-					ClassLoader cl = Thread.currentThread()
-							.getContextClassLoader();
+                try
+                {
+                    ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
-					@SuppressWarnings("unchecked")
-					Class<Controller> controllerClass = (Class<Controller>) Class
-							.forName(clazz, true, cl);
+                    @SuppressWarnings("unchecked")
+                    Class<Controller> controllerClass =
+                        (Class<Controller>) Class.forName(clazz, true, cl);
 
-					controllerClasses.put(path, controllerClass);
+                    controllerClasses.put(path, controllerClass);
 
-					LOGGER.info("Mapping {} to {}", path, controllerClass
-							.getClass().getName());
-				}
-				catch (Exception ex)
-				{
-					throw new ConfigurationException(
-							"Could not get controller class: " + clazz);
-				}
-			}
-		}
-	}
+                    LOGGER.info("Mapping {} to {}", path, controllerClass.getName());
+                }
+                catch (Exception ex)
+                {
+                    throw new ConfigurationException("Could not get controller class: " + clazz);
+                }
+            }
+        }
+    }
 }
